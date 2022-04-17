f954fb9e894083207697b3feaf7d7ec2ec526821
Adam Gausmann <agausmann@fastmail.com>
2022-04-16 23:08:42 -0500

    Parameterize Tag based on the type it represents.
    This is a nice little type-safety feature that can
    catch small logic errors at compile time, e.g.
    giving the tag to a valet of the wrong type.
    
    It comes with a small cost of added code
    complexity, since now we cannot use derives
    because they place unnecessary bounds on the
    type parameter.

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,62 @@
+use std::any::type_name;
 use std::collections::HashMap;
+use std::fmt::{self, Debug, Formatter};
+use std::hash::{Hash, Hasher};
+use std::marker::PhantomData;
 use std::ops;
 use std::sync::atomic::{AtomicU64, Ordering};
 
 static NEXT_HANDLE: AtomicU64 = AtomicU64::new(0);
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-pub struct Tag(u64);
+//XXX Don't use derives, they generate unnecessary bounds on T.
+//#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct Tag<T>(u64, PhantomData<T>);
 
-impl Tag {
+impl<T> Debug for Tag<T> {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        f.debug_tuple("Tag")
+            .field(&type_name::<T>())
+            .field(&self.0)
+            .finish()
+    }
+}
+
+impl<T> Clone for Tag<T> {
+    fn clone(&self) -> Self {
+        Self(self.0.clone(), self.1.clone())
+    }
+}
+
+impl<T> Copy for Tag<T> {}
+
+impl<T> PartialEq for Tag<T> {
+    fn eq(&self, other: &Self) -> bool {
+        self.0 == other.0 && self.1 == other.1
+    }
+}
+
+impl<T> Eq for Tag<T> {}
+
+impl<T> Hash for Tag<T> {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.0.hash(state);
+        self.1.hash(state);
+    }
+}
+
+impl<T> Tag<T> {
     fn new() -> Self {
         let val = NEXT_HANDLE.fetch_add(1, Ordering::Relaxed);
         // Prevent overflow:
         if val == u64::MAX {
             panic!("max valet tag reached")
         }
-        Self(val)
+        Self(val, PhantomData)
     }
 }
 
 pub struct Valet<T> {
-    items: HashMap<Tag, T>,
+    items: HashMap<Tag<T>, T>,
 }
 
 impl<T> Valet<T> {
@@ -29,25 +66,25 @@ impl<T> Valet<T> {
         }
     }
 
-    pub fn insert(&mut self, item: T) -> Tag {
+    pub fn insert(&mut self, item: T) -> Tag<T> {
         let handle = Tag::new();
         self.items.insert(handle.clone(), item);
         handle
     }
 
-    pub fn get(&self, handle: &Tag) -> &T {
+    pub fn get(&self, handle: &Tag<T>) -> &T {
         self.items
             .get(handle)
             .expect("handle is invalid for this depot")
     }
 
-    pub fn get_mut(&mut self, handle: &Tag) -> &mut T {
+    pub fn get_mut(&mut self, handle: &Tag<T>) -> &mut T {
         self.items
             .get_mut(handle)
             .expect("handle is invalid for this depot")
     }
 
-    pub fn remove(&mut self, handle: &Tag) -> T {
+    pub fn remove(&mut self, handle: &Tag<T>) -> T {
         self.items
             .remove(handle)
             .expect("handle is invalid for this depot")
@@ -58,16 +95,16 @@ impl<T> Valet<T> {
     }
 }
 
-impl<'a, T> ops::Index<&'a Tag> for Valet<T> {
+impl<'a, T> ops::Index<&'a Tag<T>> for Valet<T> {
     type Output = T;
 
-    fn index(&self, idx: &'a Tag) -> &Self::Output {
+    fn index(&self, idx: &'a Tag<T>) -> &Self::Output {
         self.get(idx)
     }
 }
 
-impl<'a, T> ops::IndexMut<&'a Tag> for Valet<T> {
-    fn index_mut(&mut self, idx: &'a Tag) -> &mut Self::Output {
+impl<'a, T> ops::IndexMut<&'a Tag<T>> for Valet<T> {
+    fn index_mut(&mut self, idx: &'a Tag<T>) -> &mut Self::Output {
         self.get_mut(idx)
     }
 }
